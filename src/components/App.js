ff1c1ab68524255d914053ab2b5eeb9fc1aa8780
anushkadoyan <anushkadoyan@gmail.com>
2018-01-22 21:24:54 -0800

    getting current weather for searched zip code

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,18 @@ import '../App.css';
 import Header from './Header';
 import Today from './Today';
 import OtherDays from './OtherDays';
+import axios from 'axios';
 
 class App extends Component {
   handleSearchChange(search) {
-    console.log(search);
+
+    let theWeatherNow = "http://api.openweathermap.org/data/2.5/weather?zip="+search+",us&appid=f78d4401108f0b44150be20d3e4e7146&units=imperial";
+
+    axios.get(theWeatherNow)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
+
   render() {
     return (
       <div className="App">
@@ -24,4 +31,3 @@ class App extends Component {
 export default App;
 
 
-// f78d4401108f0b44150be20d3e4e7146
